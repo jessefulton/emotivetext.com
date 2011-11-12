d05b25585affe7cd40dfc83c932b6bff4303f2f8
Jesse Fulton <jessefulton+github@gmail.com>
2011-11-12 09:44:22 -0800

    missed some files

--- src/com/eklekt/EmoTeAPIServlet.java
+++ src/com/eklekt/EmoTeAPIServlet.java
@@ -1,19 +1,66 @@
 package com.eklekt;
 
 import java.io.IOException;
+import java.util.HashMap;
 import javax.servlet.http.*;
-import synesketch.emotion.*;
+
 import com.google.gson.Gson;
 
 @SuppressWarnings("serial")
 public class EmoTeAPIServlet extends HttpServlet {
+
 	
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		Gson gson = new Gson();
+		String path = req.getPathInfo().toLowerCase();
+		resp.setContentType("application/json");
+		HashMap<String, String> responseObject = new HashMap<String, String>();
+		
+		if (path.equals("/store")) {
+			try {
+				String text = (req.getParameter("text")).toString();
+				Cookie cookie = new Cookie("text", text);
+				cookie.setMaxAge(3600);
+				resp.addCookie(cookie);
+				responseObject.put("status", "OK");
+			}
+			catch (Exception e) {
+				responseObject.put("status", "error");
+				responseObject.put("message", "please make sure cookies are enabled and your request contains the 'text' parameter");
+			}
+			finally {
+				resp.getWriter().println(gson.toJson(responseObject));
+			}
+		}
+		else if (path.startsWith("/jsonp")) {
+			try {
+				EmotionalStateWrapper emotions = null;
+				for (Cookie cookie : req.getCookies()) {
+					if (cookie.getName().equals("text")) {
+						emotions = EmotionalStateWrapper.feel(cookie.getValue());
+					}
+				}
+				
+				String callback = req.getParameter("callback");
+	
+				
+				if (callback != null) { resp.getWriter().print(callback + "("); }
+				resp.getWriter().print(gson.toJson(emotions));
+				if (callback != null) { resp.getWriter().print(");"); }
+			}
+			catch (Exception e) {
+				responseObject.put("status", "error");
+				responseObject.put("message", "please make sure cookies are enabled you have previously called the '/store' method");
+				resp.getWriter().println(gson.toJson(responseObject));
+			}
+		}
+	}
+	
+	//TODO: add cookie setting functionality to POST
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		String text = (req.getParameter("text")).toString();
-		
-		Empathyscope empathyScope = Empathyscope.getInstance();
-		EmotionalState emotions = empathyScope.feel(text);
+		String text = req.getParameter("text");
+		EmotionalStateWrapper emotions = EmotionalStateWrapper.feel(text);
 		Gson gson = new Gson();
 		
 		resp.setContentType("application/json");
